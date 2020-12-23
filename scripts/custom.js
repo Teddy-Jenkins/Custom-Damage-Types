4186bcd5e9168401584bf09522b637d7be140a66
Charles Langley <cianlangley@hotmail.com>
2020-12-22 21:05:28 -0500

    Update custom.js

--- scripts/custom.js
+++ scripts/custom.js
@@ -7,8 +7,16 @@ Hooks.once('setup', () => {
     //CONFIG.DND5E.damageTypes['fire'] = 'P5EEXTRA.DamageFire';
     //CONFIG.DND5E.damageTypes['psychic'] = 'P5EEXTRA.DamagePsychic';
     
-    CONFIG.DND5E.damageTypes['righteous'] = 'P5EEXTRA.DamageRighteous';
-    CONFIG.DND5E.damageTypes['vile'] = 'P5EEXTRA.DamageVile';
-    CONFIG.DND5E.damageTypes['arcane'] = 'P5EEXTRA.DamageArcane';
+    CONFIG.DND5E.damageTypes['righteous'] = 'Righteous';
+    CONFIG.DND5E.damageTypes['vile'] = 'Vile';
+    CONFIG.DND5E.damageTypes['arcane'] = 'Arcane';
+
+    CONFIG.DND5E.damageResistances['righteous'] = 'Righteous';
+    CONFIG.DND5E.damageResistances['vile'] = 'Vile';
+    CONFIG.DND5E.damageResistances['arcane'] = 'Arcane';
+
+    CONFIG.DND5E.damageVulnerabilities['righteous'] = 'Righteous';
+    CONFIG.DND5E.damageVulnerabilities['vile'] = 'Vile';
+    CONFIG.DND5E.damageVulnerabilities['arcane'] = 'Arcane';
     
     });
